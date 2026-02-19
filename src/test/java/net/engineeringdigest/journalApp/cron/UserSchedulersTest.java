43909a9cbc398a1fb731f0cf1db1952854dd206a
ANKIT.DEV <168528948+ANKITcodedev@users.noreply.github.com>
2026-02-19 10:17:57 +0530

    Update UserSchedulersTest.java

--- src/test/java/net/engineeringdigest/journalApp/cron/UserSchedulersTest.java
+++ src/test/java/net/engineeringdigest/journalApp/cron/UserSchedulersTest.java
@@ -1,19 +1,19 @@
-package net.engineeringdigest.journalApp.cron;
+// package net.engineeringdigest.journalApp.cron;
 
-import net.engineeringdigest.journalApp.scheduler.UserScheduler;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import net.engineeringdigest.journalApp.scheduler.UserScheduler;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class UserSchedulersTest {
+// @SpringBootTest
+// public class UserSchedulersTest {
 
-    @Autowired
-    private UserScheduler userScheduler;
+//     @Autowired
+//     private UserScheduler userScheduler;
 
 
-    @Test
-    public void testFetchUsersAndSendSaMail(){
-        userScheduler.fetchUsersAndSendSaMail();
-    }
-}
+//     @Test
+//     public void testFetchUsersAndSendSaMail(){
+//         userScheduler.fetchUsersAndSendSaMail();
+//     }
+// }
