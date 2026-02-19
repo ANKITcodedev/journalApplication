75487a18d9075049d96291a1d49502c9f8c5af46
ANKIT.DEV <168528948+ANKITcodedev@users.noreply.github.com>
2026-02-19 10:19:17 +0530

    Update RedisTests.java

--- src/test/java/net/engineeringdigest/journalApp/service/RedisTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/RedisTests.java
@@ -1,25 +1,25 @@
-package net.engineeringdigest.journalApp.service;
+// package net.engineeringdigest.journalApp.service;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
+// import org.junit.jupiter.api.Disabled;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.data.redis.core.RedisTemplate;
 
-import java.util.concurrent.TimeUnit;
+// import java.util.concurrent.TimeUnit;
 
-@SpringBootTest
-public class RedisTests {
+// @SpringBootTest
+// public class RedisTests {
 
-    @Autowired
-    private RedisTemplate redisTemplate;
+//     @Autowired
+//     private RedisTemplate redisTemplate;
 
 
-    @Disabled
-    @Test
-    void testSendMail() {
-        redisTemplate.opsForValue().set("email","gmail@email.com");
-        Object salary = redisTemplate.opsForValue().get("salary");
-        int a = 1;
-    }
-}
+//     @Disabled
+//     @Test
+//     void testSendMail() {
+//         redisTemplate.opsForValue().set("email","gmail@email.com");
+//         Object salary = redisTemplate.opsForValue().get("salary");
+//         int a = 1;
+//     }
+// }
